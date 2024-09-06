c055925ef7b442e03ebd7d0688720c30059fa994
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-06 19:38:48 +0600

    Update Peak Index in a Mountain Array.cpp

--- problem solve using STL/Peak Index in a Mountain Array.cpp
+++ problem solve using STL/Peak Index in a Mountain Array.cpp
@@ -1,1 +1,9 @@
-
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& A) {
+        
+        int i = max_element( A.begin(), A.end() ) - A.begin();
+        
+        return i;
+    }
+};
