4c63f1eb92a382db9e8d7ca59fb3d2da6a0ed860
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-07 03:06:52 +0600

    Update Max element index.cpp

--- Max_min using stl/Max element index.cpp
+++ Max_min using stl/Max element index.cpp
@@ -1,1 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>v={2,3,5,5,7,7,1};
+   int n=max_element(v.begin(),v.end())-v.begin();
+   cout<<n<<endl;
+    
+    return 0;
 
+}
+
+
+output->
+  4
