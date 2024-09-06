ed196a934431f97851d8a3d28382de1a8b88baed
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-07 03:09:05 +0600

    Update and rename Min element using index.cpp to Min element index using stl.cpp

---
+++ Max_min using stl/Min element index using stl.cpp
@@ -0,0 +1,13 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>v={2,3,5,5,7,7,1};
+   int n=min_element(v.begin(),v.end())-v.begin();
+   cout<<n<<endl;
+    
+    return 0;
+
+}
+
+
