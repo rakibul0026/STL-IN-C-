e47a500697c0637b6a0bd8d762315d93dfb0690f
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-07 02:28:56 +0600

    Update unique_vector.cpp

--- unique_vector.cpp
+++ unique_vector.cpp
@@ -1,1 +1,13 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>v={2,3,5,5,7,7,1};
+    sort(v.begin(),v.end());
+    unique(v.begin(),v.end());
+
+    for(auto u:v) cout<<u<<" ";
+    
+    return 0;
+
+}
