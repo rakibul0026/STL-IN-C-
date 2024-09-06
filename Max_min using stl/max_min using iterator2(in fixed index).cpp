654cebd9008bfa5a2410c0ca8f69f88bfc214b8b
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-07 03:02:46 +0600

    Update max_min using iterator2(in fixed index).cpp

--- Max_min using stl/max_min using iterator2(in fixed index).cpp
+++ Max_min using stl/max_min using iterator2(in fixed index).cpp
@@ -1,1 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>v={2,3,5,5,7,7,1};
+    vector<int>::iterator it=max_element(v.begin()+1,v.begin()+4);
+    cout<<*it<<endl;
+    
+    return 0;
+
+}
+
+
 
