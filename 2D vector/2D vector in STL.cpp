cc251bf57187e76a182c31c34264fa0ace34c777
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-07 03:29:25 +0600

    Update and rename p1 to 2D vector in STL.cpp

---
+++ 2D vector/2D vector in STL.cpp
@@ -0,0 +1,25 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[2][2];
+
+int main()
+{
+    vector<vector<int>> v;
+
+    vector<int> a1 = {2, 3, 4};
+    vector<int> a2 = {2, 3, 4, 5, 6};
+
+    v.push_back(a1);
+    v.push_back(a2);
+
+    for (auto u : v)
+    {
+     for (auto g :u) cout << g << " ";
+     cout << endl;
+    }
+
+    return 0;
+}
