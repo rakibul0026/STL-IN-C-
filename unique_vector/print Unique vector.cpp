1b1157d895c44de8b3d5f47abfd39a62214e02bc
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-07 02:52:16 +0600

    Update print Unique vector.cpp

--- unique_vector/print Unique vector.cpp
+++ unique_vector/print Unique vector.cpp
@@ -1,1 +1,20 @@
+//print Unique vector
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>v={2,3,5,5,7,7,1};
+    sort(v.begin(),v.end());
+    int sz=unique(v.begin(),v.end())-v.begin();
+
+    cout<<sz<<endl;
+    for(int i=0;i<sz;i++) cout<<v[i]<<" ";
+    cout<<endl;
+    
+    return 0;
+
+}
+
+  output->
+  5
+  1 2 3 5 7 
 
