e48b97825f2e4b85c030aa6357e8d6369c52e48c
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-07 18:00:17 +0600

    Update Erase and back.cpp

--- Vector in C++ STL/Erase and back.cpp
+++ Vector in C++ STL/Erase and back.cpp
@@ -1,1 +1,15 @@
+//erase is also delete the element
+//.back is also print the last element
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>v={2,3,4,5};
 
+    cout<<v.back()<<endl;  //v.back  also print the last element 
+    v.pop_back();  //v.pop_back also delete the last element
+    cout<<v.back()<<endl;
+
+    cout<<*v.begin()<<endl;// *v.begin the first element
+    v.erase(v.begin()); //  v.erase deleting th first element
+    cout<<*v.begin()<<endl;
+}
