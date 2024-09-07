a2357c9dbf3ad81135478b3cd09ffa17b515fe6e
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-07 18:03:37 +0600

    Update vector input and print.cpp

--- Vector in C++ STL/vector input and print.cpp
+++ Vector in C++ STL/vector input and print.cpp
@@ -1,1 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int>v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(4);
+    v.push_back(5);
+    v.push_back(9);
 
+    cout<<v[2]<<endl;
+    cout<<v.size()<<endl;
+
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<endl;
+
+    }
+}
