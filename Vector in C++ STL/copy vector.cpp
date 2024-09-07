b87b2fc5c3cc9429b387663f3ff7b390c25d39b2
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-07 17:56:47 +0600

    Update copy vector.cpp

--- Vector in C++ STL/copy vector.cpp
+++ Vector in C++ STL/copy vector.cpp
@@ -1,1 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>v={2,3,4,5,6,7};
+    vector<int>temp;
 
+    temp=v;
+    for(int i=0;i<temp.size();i++){
+        cout<<v[i]<<" ";
+    }
+
+}
