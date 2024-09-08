1bb4d6196898c7119006a84bfb0f86d859582afb
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-09 01:12:27 +0600

    Update 6.A. Haiku(codeforce).cpp

--- STL String and problem solving/6.A. Haiku(codeforce).cpp
+++ STL String and problem solving/6.A. Haiku(codeforce).cpp
@@ -1,3 +1,48 @@
+/*
+   
+                                                                A. Haiku
+                                                         time limit per test2 seconds
+                                                       memory limit per test256 megabytes
+Haiku is a genre of Japanese traditional poetry.
+
+A haiku poem consists of 17 syllables split into three phrases, containing 5, 7 and 5 syllables correspondingly (the first phrase should contain 
+exactly 5 syllables, the second phrase should contain exactly 7 syllables, and the third phrase should contain exactly 5 syllables). A haiku
+masterpiece contains a description of a moment in those three phrases. Every word is important in a small poem, which is why haiku are rich with
+symbols. Each word has a special meaning, a special role. The main principle of haiku is to say much using a few words.
+
+To simplify the matter, in the given problem we will consider that the number of syllable in the phrase is equal to the number of vowel letters there. 
+Only the following letters are regarded as vowel letters: "a", "e", "i", "o" and "u".
+
+Three phases from a certain poem are given. Determine whether it is haiku or not.
+
+Input
+The input data consists of three lines. The length of each line is between 1 and 100, inclusive. The i-th line contains the i-th phrase of the poem. Each
+phrase consists of one or more words, which are separated by one or more spaces. A word is a non-empty sequence of lowercase Latin letters. Leading and/or
+trailing spaces in phrases are allowed. Every phrase has at least one non-space character. See the example for clarification.
+
+Output
+Print "YES" (without the quotes) if the poem is a haiku. Otherwise, print "NO" (also without the quotes).
+
+Examples
+Input
+on  codeforces 
+beta round is running
+   a rustling of keys 
+Output
+YES
+
+
+Input
+how many gallons
+of edo s rain did you drink
+                                cuckoo
+Output
+NO
+
+*/
+
+
+
 /// In the name of ALLAH
 
 #include<bits/stdc++.h>
@@ -8,26 +53,35 @@ using namespace std;
 
 bool isVowel ( char c )
 {
-    return ( c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y'  );
+    return ( c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' );
 }
 
 int main()
 {
     optimize();
 
-    string s, ans;
+    vector<string> v;
+    int cnt[] = { 5, 7, 5 };
 
-    cin >> s;
+    for ( int i = 0; i < 3; i++ ) {
+        string s;
+        char c;
+        cin >> c;
+        getline ( cin, s );
+        s = c + s;
 
-    for ( auto u : s ) {
-        char c = tolower ( u );
-
-        if ( isVowel ( c ) == 0 ) {
-            ans += '.';
-            ans += c;
-        }
+        v.push_back ( s );
     }
 
-    cout << ans << endl;
+    for ( int i = 0; i < 3; i++ ) {
+        int c = 0;
 
+        for ( auto u : v[i] ) {
+            if ( isVowel(u) ) c++;
+        }
+
+        if ( c != cnt[i] ) return cout << "NO\n", 0;
+    }
+
+    cout << "YES\n";
 }
