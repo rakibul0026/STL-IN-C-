0831cab31f00fb0d93438cff0886c58b550bd197
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-08 15:53:24 +0600

    Update 8.Discovering Permutations.cpp

--- STL String and problem solving/8.Discovering Permutations.cpp
+++ STL String and problem solving/8.Discovering Permutations.cpp
@@ -1,1 +1,42 @@
 
+/*In this problem you have to find the permutations using the first N English capital letters. Since there can be many permutations,
+you have to print the first K.
+
+Input
+Input starts with an integer T (≤ 100), denoting the number of test cases.
+
+Each case contains two integers N, K (1 ≤ N ≤ 26, 1 ≤ K ≤ 30).
+
+Output
+For each case, print the case number in a line. Then print the first K permutations that contain the first N English capital letters in
+alphabetical order. If there are less than K permutations then print all of them.
+
+Sample
+Input
+2
+3 8
+10 10
+
+Output
+Case 1:
+ABC
+ACB
+BAC
+BCA
+CAB
+CBA
+Case 2:
+ABCDEFGHIJ
+ABCDEFGHJI
+ABCDEFGIHJ
+ABCDEFGIJH
+ABCDEFGJHI
+ABCDEFGJIH
+ABCDEFHGIJ
+ABCDEFHGJI
+ABCDEFHIGJ
+ABCDEFHIJG
+
+  */
+
+
