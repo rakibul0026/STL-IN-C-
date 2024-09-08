cbb775ac8084819a22eb72140d5ce9385ab65610
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-09 01:33:08 +0600

    Update 5. A. Petya and Strings(codeforce).cpp

--- STL String and problem solving/5. A. Petya and Strings(codeforce).cpp
+++ STL String and problem solving/5. A. Petya and Strings(codeforce).cpp
@@ -36,6 +36,30 @@ Note
 If you want more formal information about the lexicographical order (also known as the "dictionary order" or "alphabetical order"), you can 
 visit the following site:*/
 
+/// In the name of ALLAH
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+
+int main()
+{
+    optimize();
+
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    for ( int i = 0; i < s1.size(); i++ ) s1[i] = tolower ( s1[i] );
+    for ( int i = 0; i < s2.size(); i++ ) s2[i] = tolower ( s2[i] );
+
+    if ( s1 < s2 ) cout << "-1";
+    else if ( s1 > s2 ) cout << "1";
+    else cout << "0";
+
+}
 
 
 
