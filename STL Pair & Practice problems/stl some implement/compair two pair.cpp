9c1784b09651f58315981c1026fc59ac91007ba2
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-12 22:47:01 +0600

    Update compair two pair.cpp

--- STL Pair & Practice problems/stl some implement/compair two pair.cpp
+++ STL Pair & Practice problems/stl some implement/compair two pair.cpp
@@ -1,1 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   pair<int,int>p1,p2;
+   p1={8,3};
+   p2={1,1};
+
+   if(p1<p2)cout<<"YES\n";
+   else cout<<"NO\n";
+  
+
+    return 0;
+}
 
