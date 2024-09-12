466aae26b468dbfb0a9af2fcd956a3d0cbbac83f
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-12 22:45:51 +0600

    Create p5.cpp

---
+++ STL Pair & Practice problems/stl some implement/p5.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   pair<string,vector<int>>p;
+   p={"rakibul",{1,2,3,4,5}};
+
+  cout<<p.first<<" "<<p.second.size()<<endl;
+
+    return 0;
+}
