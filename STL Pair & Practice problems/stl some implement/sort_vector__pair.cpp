4060879f506f8a27a8ad26267fb30cbca4f300d7
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-12 22:51:11 +0600

    Update sort_vector__pair.cpp

--- STL Pair & Practice problems/stl some implement/sort_vector__pair.cpp
+++ STL Pair & Practice problems/stl some implement/sort_vector__pair.cpp
@@ -1,1 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    pair<int, int> p[] = {{6, 5}, {2, 3}, {4, 5}, {6, 1}, {1, 9}};
+    sort(p, p + 5);
+    for (auto u : p)
+    {
+        cout << u.first << " " << u.second << endl;
+    }
+}
 
