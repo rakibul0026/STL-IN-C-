2672969e9a3faa7c1b7114d892e20d17017a0986
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-12 16:16:17 +0600

    Update 3) A. Rank List solve use comparator.cpp

--- STL Pair & Practice problems/3) A. Rank List solve use comparator.cpp
+++ STL Pair & Practice problems/3) A. Rank List solve use comparator.cpp
@@ -1,1 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool cmp(const pair<int, int>& p1, const pair<int, int>& p2) {
+    if (p1.first > p2.first) return true;
+    if (p1.first == p2.first) return p1.second < p2.second;  
+    return false;
+}
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<pair<int, int>> v(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> v[i].first >> v[i].second;
+    }
+
+    sort(v.begin(), v.end(), cmp);  
+
+    int ans = 0;
+
+    for (auto u : v) {
+        if (u == v[k - 1])
+            ans++;
+    }
+
+    cout << ans << endl;
+    return 0;
+}
 
