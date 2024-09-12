9ff923e05800c9a64d16d3c8cbb35f84efad49ca
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-12 22:43:41 +0600

    Update p1.cpp

--- STL Pair & Practice problems/stl some implement/p1.cpp
+++ STL Pair & Practice problems/stl some implement/p1.cpp
@@ -1,1 +1,9 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    pair<string,int>p;
+    p.first="rakibul";
+    p.second=26;
+    cout<<p.first<<" "<<endl;
+    cout<<p.second<<" ";
+}
