251a274cd3f80e0d9786882511785109ce43b7e1
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-12 22:44:09 +0600

    Update p2.cpp

--- STL Pair & Practice problems/stl some implement/p2.cpp
+++ STL Pair & Practice problems/stl some implement/p2.cpp
@@ -1,1 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    pair<string, vector<int>> p; 
+
+    p.first = "rakibul";
+    p.second = {1, 2, 3, 4}; 
+
+    cout << p.first << " " << endl;
+
+    for (auto u : p.second) {
+        cout << u << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
 
