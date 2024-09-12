5e5b6e21c6b2b6910d7afd096024ab16bed84252
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-12 22:44:42 +0600

    Create p3.cpp

---
+++ STL Pair & Practice problems/stl some implement/p3.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    pair<int,int>p;
+
+    p=make_pair(2,3);
+    p.first++;
+    
+    cout<<p.first<<" "<<p.second<<endl;
+
+    return 0;
+}
