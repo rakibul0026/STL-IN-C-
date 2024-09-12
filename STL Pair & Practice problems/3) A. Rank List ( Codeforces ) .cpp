f47716f0fb30c2f010bb593a30a3f814d1330b23
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-12 16:07:18 +0600

    Update 3) A. Rank List ( Codeforces ) .cpp

--- STL Pair & Practice problems/3) A. Rank List ( Codeforces ) .cpp
+++ STL Pair & Practice problems/3) A. Rank List ( Codeforces ) .cpp
@@ -1,1 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<pair<int, int>> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i].first >> v[i].second;
+        v[i].first *= -1;
+    }
+
+    sort(v.begin(), v.end());
+
+    int ans = 0;
+
+    for (auto u : v)
+    {
+        if (u == v[k - 1])
+            ans++;
+    }
+
+    cout << ans << endl;
+}
