fb618b5d2b2b3ec609a71a6af441331c75897b87
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-14 01:55:54 +0600

    Create example2 .cpp

---
+++ STL Map with practice problems /1) STL Map & Practice problems (Part-1)/example2 .cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    map<string,string>gender;
+    
+    gender["rakibul"]="male";
+    gender["borsha"]="female";
+
+    cout<<gender["rakibul"]<<" "<<gender["borsha"]<<endl;
+
+}
