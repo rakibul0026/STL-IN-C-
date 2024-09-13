d2a2f46f7d0f3c96ae52de540d2876ca59cf0b55
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-14 01:54:01 +0600

    Update unique_map.cpp

--- STL Map with practice problems /1) STL Map & Practice problems (Part-1)/unique_map.cpp
+++ STL Map with practice problems /1) STL Map & Practice problems (Part-1)/unique_map.cpp
@@ -1,1 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   map<int, int> c;
+   c[1] = 1;
+   c[2] = 2;
+   c[3] = 1;
+   c[3] = 2;
+
+   cout << c.size() << endl;
+
+   return 0;
+}
 
