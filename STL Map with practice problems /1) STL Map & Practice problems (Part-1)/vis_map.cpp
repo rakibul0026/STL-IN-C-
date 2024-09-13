13164b4b1696fbf970861218f2efa94b00328def
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-14 01:53:12 +0600

    Update vis_map.cpp

--- STL Map with practice problems /1) STL Map & Practice problems (Part-1)/vis_map.cpp
+++ STL Map with practice problems /1) STL Map & Practice problems (Part-1)/vis_map.cpp
@@ -1,1 +1,15 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+string d[123];
+
+int main()
+{
+    map<int ,bool>vis;
+    vector<int>v={2,2,1,1,3};
+    for(auto u:v) vis[u]=1;
+
+    for(auto u: vis) cout<<u.first<<" "<<u.second<<endl;
+
+    return 0;
+}
