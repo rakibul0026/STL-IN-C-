821fb9cfba0313bbef77184161287de02a80067a
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-14 01:57:56 +0600

    Create map_example.cpp

---
+++ STL Map with practice problems /1) STL Map & Practice problems (Part-1)/map_example.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    map<string,int>id;
+
+    id["shahriar"]=1;
+    id["momo"]=3;
+    id["sharif"]=4;
+    id["prety"]=9;
+
+   for(auto u:id)cout<<u.first<<" "<<u.second<<endl;
+
+}
