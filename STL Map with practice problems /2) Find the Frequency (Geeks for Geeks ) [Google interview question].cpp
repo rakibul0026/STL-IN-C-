8e13ff37d2d3287281b5f45f38ff90c2caf22b2e
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-14 01:41:00 +0600

    Update 2) Find the Frequency (Geeks for Geeks ) [Google interview question].cpp

--- STL Map with practice problems /2) Find the Frequency (Geeks for Geeks ) [Google interview question].cpp
+++ STL Map with practice problems /2) Find the Frequency (Geeks for Geeks ) [Google interview question].cpp
@@ -1,1 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
 
+// } Driver Code Ends
+
+class Solution {
+public:
+    map<string, int> cnt;
+
+    int countWords(string list[], int n) {
+        int ans = 0;
+
+        // Count the frequency of each word in the list
+        for (int i = 0; i < n; i++) {
+            cnt[list[i]]++;
+        }
+
+        // Count how many words appear exactly twice
+        for (auto u : cnt) {
+            if (u.second == 2) ans++;
+        }
+
+        return ans;
+    }
+};
+
+   
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string list[n];
+        for(int i=0;i<n;i++)
+            cin>>list[i];
+        Solution ob;    
+        cout <<ob.countWords(list, n)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
