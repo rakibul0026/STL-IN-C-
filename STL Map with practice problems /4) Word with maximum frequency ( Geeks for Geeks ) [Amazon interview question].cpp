6c19c281fd4c4ac17f734ee6b4f8d2904aa04521
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-15 00:10:47 +0600

    Update 4) Word with maximum frequency ( Geeks for Geeks ) [Amazon interview question].cpp

--- STL Map with practice problems /4) Word with maximum frequency ( Geeks for Geeks ) [Amazon interview question].cpp
+++ STL Map with practice problems /4) Word with maximum frequency ( Geeks for Geeks ) [Amazon interview question].cpp
@@ -1,3 +1,20 @@
+/*                                        Word with maximum frequency
+
+You are given a string str that is made up of words separated by spaces. Your task is to find the word with the highest frequency, i.e. it
+appears the most times in the sentence. If multiple words have maximum frequency, then print the word that occurs first in the sentence.
+
+Examples:
+
+Input: str = "the devil in the sky"
+Output: the 2
+Explanation: The frequency of "the" is 2, so we return "the" and its frequency "2" i.e., "the 2" 
+
+Input: str = "this is not right"
+Output: this 1
+Explanation: Every word has the frequency of "1", so we return "this 1" as this occurs first in the sentence. */
+
+
+
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
