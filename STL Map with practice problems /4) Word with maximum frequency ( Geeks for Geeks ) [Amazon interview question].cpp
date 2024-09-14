1ed4edcd73b465ac9bd9df69993de151ca200680
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-14 13:34:56 +0600

    Update 4) Word with maximum frequency ( Geeks for Geeks ) [Amazon interview question].cpp

--- STL Map with practice problems /4) Word with maximum frequency ( Geeks for Geeks ) [Amazon interview question].cpp
+++ STL Map with practice problems /4) Word with maximum frequency ( Geeks for Geeks ) [Amazon interview question].cpp
@@ -1,1 +1,35 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    string s;
+    getline ( cin, s );
 
+    string tmp;
+    vector < string > v;
+    for( auto i : s){
+        if( isspace(i) ){
+            v.push_back( tmp );
+            tmp.clear();
+        }
+        else tmp += i;
+    }
+    v.push_back( tmp );
+
+    map < string, int > m;
+    int maxFrq = 0;
+    for( auto i : v ){
+        m[i]++;
+        maxFrq = max ( maxFrq, m[i] );
+    }
+
+    string ans;
+    for( auto i : m){
+        if( i.second == maxFrq ){
+            ans = i.first;
+            break;
+        }
+    }
+    cout << ans << " " << maxFrq << endl;
+}
