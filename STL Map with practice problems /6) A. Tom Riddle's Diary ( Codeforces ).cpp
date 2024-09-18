c4b9030b7e3c92dd3f2d0032ffe1afa01eda02ee
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-19 00:02:02 +0600

    Update 6) A. Tom Riddle's Diary ( Codeforces ).cpp

--- STL Map with practice problems /6) A. Tom Riddle's Diary ( Codeforces ).cpp
+++ STL Map with practice problems /6) A. Tom Riddle's Diary ( Codeforces ).cpp
@@ -1,27 +1,16 @@
-/// In the name of ALLAH
-
 #include<bits/stdc++.h>
 using namespace std;
-
-#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-
-int main()
-{
-
-    optimize();
-
+int main(){
     int n;
-    cin >> n;
+    cin>>n;
 
-    map<string, bool> vis;
-
-    while ( n-- ) {
+    map<string,bool>vis;
+    while(n--){
         string s;
-        cin >> s;
-        if ( vis[s] == 1 ) cout << "YES\n";
-        else cout << "NO\n";
+        cin>>s;
+        if(vis[s]==1)cout<<"YES\n";
+        else cout<<"NO\n";
 
-        vis[s] = 1;
+        vis[s]=1;
     }
-
 }
