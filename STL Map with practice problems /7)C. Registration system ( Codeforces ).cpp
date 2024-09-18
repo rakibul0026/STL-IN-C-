d0aa93b3dbd6c473fabfbbfb2aceec5578b06d9b
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-19 00:00:23 +0600

    Update 7)C. Registration system ( Codeforces ).cpp

--- STL Map with practice problems /7)C. Registration system ( Codeforces ).cpp
+++ STL Map with practice problems /7)C. Registration system ( Codeforces ).cpp
@@ -1,28 +1,21 @@
-/// In the name of ALLAH
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-
 int main()
 {
-
-    optimize();
-
     int n;
     cin >> n;
 
     map<string, int> cnt;
 
-    while ( n-- ) {
+    while (n--)
+    {
         string s;
         cin >> s;
+        if (cnt[s] == 0)
+            cout << "OK\n";
+        else
+            cout << s << cnt[s] <<endl;
 
-        if ( cnt[s] == 0 ) cout << "OK\n";
-        else cout << s << cnt[s] << endl;
-
-        cnt[s]++;
+            cnt[s]++;
     }
-
 }
