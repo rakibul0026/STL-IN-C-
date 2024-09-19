c6e542be44fcb6ec98fe60e416611345c8f0b929
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-20 00:20:58 +0600

    Update 5) A. I Wanna Be the Guy ( Codeforces ).cpp

--- STL Set & Practice problems/5) A. I Wanna Be the Guy ( Codeforces ).cpp
+++ STL Set & Practice problems/5) A. I Wanna Be the Guy ( Codeforces ).cpp
@@ -1,1 +1,31 @@
+///In the name of ALLAH
 
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    set<int> s;
+    int n;
+    cin >> n;
+
+    int p;
+    cin >> p;
+    for ( int i = 0; i < p; i++ ) {
+        int a;
+        cin >> a;
+        s.insert ( a );
+    }
+
+    
+    cin >> p;
+    for ( int i = 0; i < p; i++ ) {
+        int a;
+        cin >> a;
+        s.insert ( a );
+    }
+
+    if ( s.size () == n ) cout << "I become the guy.\n";
+    else cout << "Oh, my keyboard!\n";
+}
