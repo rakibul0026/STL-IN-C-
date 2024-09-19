f814fcdc74c573644eb9d87c5ef35d5fa3ca06cd
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-20 00:21:33 +0600

    Update 7) A. Second Order Statistics ( Codeforces ).cpp

--- STL Set & Practice problems/7) A. Second Order Statistics ( Codeforces ).cpp
+++ STL Set & Practice problems/7) A. Second Order Statistics ( Codeforces ).cpp
@@ -1,1 +1,22 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    set<int> s;
+    int n;
+    cin >> n;
+
+    for ( int i = 0; i < n; i++ ) {
+        int a;
+        cin >> a;
+        s.insert ( a );
+    }
+
+    if ( s.size() == 1 ) cout << "NO\n";
+    else {
+        cout << *( ++s.begin() ) << endl;
+    }
+}
