40e109eb5e7f117ddb63d0421f73ce594e801422
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-20 17:40:26 +0600

    Create one occurance erase.cpp

---
+++ STL Multiset and practice problem /one occurance erase.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    multiset<int> s;
+    s.insert(1);
+    s.insert(2);
+    s.insert(1);
+    s.insert(3);
+    s.insert(1);
+    s.insert(4);
+    s.insert(1);
+    s.insert(4);
+    
+    auto it=s.find(1);
+    s.erase(it);
+
+    for(auto u:s)cout<<u<<" ";
+    cout<<endl;
+}
