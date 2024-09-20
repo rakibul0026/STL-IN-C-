4ecbd0eb1c6d6ef4476d2e2367dc91408f25a569
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-20 17:35:57 +0600

    Update stl multiset erase.cpp

--- STL Multiset and practice problem /stl multiset erase.cpp
+++ STL Multiset and practice problem /stl multiset erase.cpp
@@ -1,1 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    multiset<int> s;
+    s.insert(1);
+    s.insert(2);
+    s.insert(1);
+    s.insert(3);
+    s.insert(1);
+    s.insert(4);
+    s.insert(1);
+    s.insert(4);
+    
+    s.erase(1);
+    for(auto u:s)cout<<u<<" ";
+    cout<<endl;
+}
