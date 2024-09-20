7a5b19cbd8aafea179c5b08b106fe49ae8acb7e4
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-20 17:31:14 +0600

    Update p1.c

--- STL Multiset and practice problem /p1.c
+++ STL Multiset and practice problem /p1.c
@@ -1,1 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    multiset<int> s;
+    s.insert(1);
+    s.insert(2);
+    s.insert(1);
+    s.insert(3);
+    s.insert(1);
+    s.insert(4);
+    s.insert(1);
+    s.insert(4);
+    
+    cout<<s.count(1)<<endl;
+}
