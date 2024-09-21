8e10c0591f1e16edf1da9e3476b499a422a37d2c
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-22 00:20:58 +0600

    Update 5) 1047. Remove All Adjacent Duplicates In String ( LeetCode ).cpp

--- 1) STL Stack & Practice problems/5) 1047. Remove All Adjacent Duplicates In String ( LeetCode ).cpp
+++ 1) STL Stack & Practice problems/5) 1047. Remove All Adjacent Duplicates In String ( LeetCode ).cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    string removeDuplicates(string S) {
+        stack<char> stk;
+        for (char ch : S) {
+            if (!stk.empty() && stk.top() == ch) {
+                stk.pop();
+            } else {
+                stk.push(ch);
+            }
+        }
+        string result;
+        while (!stk.empty()) {
+            result += stk.top();
+            stk.pop();
+        }
+        reverse(result.begin(), result.end()); 
+        return result;
+    }
+};
 
