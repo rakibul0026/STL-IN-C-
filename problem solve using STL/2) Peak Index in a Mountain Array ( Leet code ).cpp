c94ba1bbe8da3855925dd0d77190bb688334842e
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-21 10:52:08 +0600

    Delete problem solve using STL directory

--- problem solve using STL/2) Peak Index in a Mountain Array ( Leet code ).cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int peakIndexInMountainArray(vector<int>& A) {
-        
-        int i = max_element( A.begin(), A.end() ) - A.begin();
-        
-        return i;
-    }
-};
