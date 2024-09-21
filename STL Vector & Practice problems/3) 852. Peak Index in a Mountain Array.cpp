782aa7d498f0dff64d12c83826ce7e8d109f7114
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-21 10:28:38 +0600

    Update 3) 852. Peak Index in a Mountain Array.cpp

--- STL Vector & Practice problems/3) 852. Peak Index in a Mountain Array.cpp
+++ STL Vector & Practice problems/3) 852. Peak Index in a Mountain Array.cpp
@@ -1,1 +1,10 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& A) {
+        
+        int i = max_element( A.begin(), A.end() ) - A.begin();
+        
+        return i;
+    }
+};
 
