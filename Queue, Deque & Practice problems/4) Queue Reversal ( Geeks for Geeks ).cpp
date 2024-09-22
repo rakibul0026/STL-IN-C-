5569e36117f093b033ac83214f7a2e720c0f7d0c
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-22 11:13:26 +0600

    Update 4) Queue Reversal ( Geeks for Geeks ).cpp

--- Queue, Deque & Practice problems/4) Queue Reversal ( Geeks for Geeks ).cpp
+++ Queue, Deque & Practice problems/4) Queue Reversal ( Geeks for Geeks ).cpp
@@ -1,1 +1,83 @@
+/*                             Queue Reversal
 
+Given a Queue Q containing N elements. The task is to reverse the Queue. Your task is to complete the 
+function rev(), that reverses the N elements of the queue.
+
+Example 1:
+
+Input:
+6
+4 3 1 10 2 6
+Output: 
+6 2 10 1 3 4
+Explanation: 
+After reversing the given elements of the queue , the resultant queue will be 6 2 10 1 3 4.
+Example 2:
+
+Input:
+4
+4 3 2 1 
+Output: 
+1 2 3 4
+Explanation: 
+After reversing the given elements of the queue , the resultant queue will be 1 2 3 4.*/
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//function Template for C++
+
+//Function to reverse the queue.
+class Solution
+{
+    public:
+    queue<int> rev(queue<int> q)
+    {
+        stack<int> st;
+        
+        // Step 1: Push all elements of the queue into the stack
+        while (!q.empty()) {
+            st.push(q.front());
+            q.pop();
+        }
+        
+        // Step 2: Pop all elements from the stack and push them back into the queue
+        while (!st.empty()) {
+            q.push(st.top());
+            st.pop();
+        }
+          return q;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int test;
+    cin>>test; 
+    while(test--)
+    {
+    queue<int> q; 
+    int n, var; 
+    cin>>n; 
+    while(n--)
+    {
+        cin>>var; 
+        q.push(var);
+    }
+    Solution ob;
+    queue<int> a=ob.rev(q); 
+    while(!a.empty())
+    {
+        cout<<a.front()<<" ";
+        a.pop();
+    }
+    cout<<endl; 
+    }
+}
+// } Driver Code Ends
