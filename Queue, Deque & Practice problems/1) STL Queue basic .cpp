808fcad566952a294d7c1c676de642e779711046
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-22 09:29:35 +0600

    Update 1) STL Queue basic .cpp

--- Queue, Deque & Practice problems/1) STL Queue basic .cpp
+++ Queue, Deque & Practice problems/1) STL Queue basic .cpp
@@ -1,1 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    queue<int> q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(5);
+
+    cout<<q.size()<<endl;
+
+    cout << q.front() << " " << endl;
+
+    while (!q.empty())
+    {
+        cout << q.front();
+        q.pop();
+    }
+}
