d693e31130d4962d8f0cd1c72eb66d9b55e3f0da
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-23 12:06:56 +0600

    Update min_priority_queue_example.cpp

--- STL Priority queue & Practice problem/1) STL Priority Queue all basic tool/min_priority_queue_example.cpp
+++ STL Priority queue & Practice problem/1) STL Priority Queue all basic tool/min_priority_queue_example.cpp
@@ -1,1 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    priority_queue<int,vector<int>,greater<int>>q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);   // priority_queue always done  log2(n)
+    q.push(5);
+
+    cout<<q.size()<<endl;
+
+  
+
+    while (!q.empty())
+    {
+        cout << q.top()<<" ";
+        q.pop();
+    }
+}
