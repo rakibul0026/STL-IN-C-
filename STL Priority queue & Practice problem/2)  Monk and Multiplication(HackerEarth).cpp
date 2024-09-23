762b966273d90a7cb1284bea13f12cc2edfde55b
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-23 16:00:28 +0600

    Update 2)  Monk and Multiplication(HackerEarth).cpp

--- STL Priority queue & Practice problem/2)  Monk and Multiplication(HackerEarth).cpp
+++ STL Priority queue & Practice problem/2)  Monk and Multiplication(HackerEarth).cpp
@@ -1,1 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n); 
+    priority_queue<int> q;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        q.push(arr[i]);
+        if (q.size() >= 3)
+        {
+         
+            int a1 = q.top();
+            q.pop();
+            int a2 = q.top();
+            q.pop();
+            int a3 = q.top();
+            q.pop();
+
+            int sum = a1 * a2 * a3;
+            cout << sum << endl;
+
+           
+            q.push(a1);
+            q.push(a2);
+            q.push(a3);
+        }
+        else
+        {
+            cout << -1 << endl;
+        }
+    }
+}
 
